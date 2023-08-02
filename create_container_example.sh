1215e2ecb9e01fa478c1cbf02c6e03eb892336d9
Miloš Čadek <34865543+c4da@users.noreply.github.com>
2023-08-02 22:22:01 +0200

    Update create_container_example.sh

--- create_container_example.sh
+++ create_container_example.sh
@@ -1,6 +1,7 @@
 #this is not a runnable sh file
-docker-compose -f docker-compose.yml up
+docker-compose -f docker-compose.yml up #creates container according to yaml
 docker ps
-docker exec -it <id> bash
-psql -U postgres -d dev -h localhost -f schema.sql
-psql -U postgres -d dev -h localhost -f data.sql
+docker exec -it <id> bash #connecting to container in a different terminal
+psql -U postgres -d dev -h localhost -f schema.sql #run sql script to create table
+psql -U postgres -d dev -h localhost -f data.sql  #run sql script to fill table
+docker inspect <id>
